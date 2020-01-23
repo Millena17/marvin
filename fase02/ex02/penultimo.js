58acfa29b2d6ed721c80575ce7156d088eabf7bf
Millena17 <59873090+Millena17@users.noreply.github.com>
2020-01-23 08:32:11 -0300

    fase2 ex 3

--- fase02/ex02/penultimo.js
+++ fase02/ex02/penultimo.js
@@ -1,6 +1,4 @@
-
 function penultimo(palavra) {
-    var palavras = palavra;
+    const palavras = palavra;
     return palavras.substring(8,7)
-    //isto serve apenas para uma string com 6 letras
 }
