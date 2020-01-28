eba748d8ba945aa95b47cd5d716c923471b2c5da
Millena17 <59873090+Millena17@users.noreply.github.com>
2020-01-28 10:30:11 -0300

    tres primeiras letras maiúsculas

--- fase02/ex03/maiusculo.js
+++ fase02/ex03/maiusculo.js
@@ -1,4 +1,4 @@
 function maiusculo(palavra) {
     const palavras = palavra;
-    return palavras.substring(3,0).toUpperCase().concat(3)
+    return palavras.substring(3,0).toUpperCase().concat()
 }
