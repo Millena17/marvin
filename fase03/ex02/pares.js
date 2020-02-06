050e7d0b633861fc4285123288c2421e47e0c2a0
Millena17 <59873090+Millena17@users.noreply.github.com>
2020-02-06 09:18:28 -0300

    pares

---
+++ fase03/ex02/pares.js
@@ -0,0 +1,9 @@
+function pares(num1,num2){
+  var numero = num1,num2 
+  var total = numero%2;
+ if(total=== 0){
+  return ("os numeros são pares")
+} else {
+ } return ("os numeros não são pares")
+}
+
