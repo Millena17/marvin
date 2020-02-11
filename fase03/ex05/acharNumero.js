286b2c2effb1c3c4c760f54a2ef78b5be5e7ba18
Millena17 <59873090+Millena17@users.noreply.github.com>
2020-02-11 08:38:42 -0300

    dona millena

--- fase03/ex05/acharNumero.js
+++ fase03/ex05/acharNumero.js
@@ -1,18 +1,13 @@
 
-function acharNumero ( num ) {
-    const  numero  =  num
-    if ( numero  <  5 ){
-        return( "mercurio" )
-    }
-     else if(numero >= 5  &&  numero <= 10){
-        return ( "venus" )
-    }
-     else if(numero  > 10 &&  numero <= 100) {
-        return ("terra")
+function acharNumero (numero) {
+    if (numero  <5){
+      return "mercurio"; 
+         }else if(numero== 5 || numero <= 10){
+        return  "venus";
+         }else if(numero <= 100) {
+      return "terra";
     }
     else{
-        return ("marte")
+        return "marte";
     }
 }
-
-
