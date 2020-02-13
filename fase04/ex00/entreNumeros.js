e466559c1c731db7b6f46b76e7896906c3c5d0cb
Millena17 <59873090+Millena17@users.noreply.github.com>
2020-02-13 09:31:27 -0300

    marvin

--- fase04/ex00/entreNumeros.js
+++ fase04/ex00/entreNumeros.js
@@ -1,19 +1,7 @@
-function fizzbuzz(num1,num2){
-    var c = num1
-    while(c <= num2){
-           if(c % 3 === 0){
-        console.log(c + "Fizz")
-        c++
-    }
-    if(c % 5 === 0){
-        console.log(c + "Buzz")
-        c++
-    }
-    if(c % 3 === 0 && c % 5=== 0){
-     console.log( + "FizzBuzz")
-     c++
-    }
-    console.log(c)
-    c++
-    }
+function entreNumeros(min, max){
+while (min<=max){
+console.log(min,max) 
+min++
+    
+}
 }
